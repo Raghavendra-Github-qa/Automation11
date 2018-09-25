f59eaf2254df54fa8434159192ae31c9eb12674e
Raghavendra-Github-qa <43536509+Raghavendra-Github-qa@users.noreply.github.com>
2018-09-25 19:36:19 +0530

    Classes Demo2 and Demo3 are deleted

--- src/practice/Demo2.java
+++
@@ -1,9 +0,0 @@
-package practice;
-
-public class Demo2 {
-
-	public static void main(String[] args) {
-		int i=10;
-		System.out.println(i);
-	}
-}
--- src/practice/Demo3.java
+++
@@ -1,8 +0,0 @@
-package practice;
-
-public class Demo3 {
-
-	public static void main(String[] args) {
-		System.out.println("This is Demo3 class");
-	}
-}
